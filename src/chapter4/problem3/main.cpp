3abcddcda2e71c803180538828c8c1fd781c5069
rinechran <rinechran@outlook.jp>
2020-03-13 16:52:13 +0900

    feat : problem3

--- src/chapter4/problem3/main.cpp
+++ src/chapter4/problem3/main.cpp
@@ -1,4 +1,4 @@
-#include <cstdio>
+#include <iostream>
 
 using namespace std;
 
@@ -11,12 +11,12 @@ using namespace std;
  * @return
  */
 long long getGCD(long long a, long long b) {
-    while (a % b != 0) {
+    while (b != 0) {
         int temp = a % b;
         a = b;
         b = temp;
     }
-    return b;
+    return a;
 }
 
 /**
@@ -27,25 +27,24 @@ long long getGCD(long long a, long long b) {
  * @return
  */
 long long getLCM(long long a, long long b) {
-    return a* b / getGCD(a, b);
+    return a * b / getGCD(a, b);
 }
 
 void process(int caseIndex) {
-	int num1, num2;
-	scanf("%d%d", &num1, &num2);
+    int num1, num2;
+    std::cin >> num1, num2;
+    long long gcd = getGCD(num1, num2);
+    long long lcm = getLCM(num1, num2);
 
-	long long gcd = getGCD(num1, num2);
-	long long lcm = getLCM(num1, num2);
-
-	printf("Case #%d:\n", caseIndex);
-	printf("%lld %lld\n", gcd, lcm);
+    printf("Case #%d:\n", caseIndex);
+    printf("%lld %lld\n", gcd, lcm);
 }
 
 int main() {
-	int caseSize;
-	scanf("%d", &caseSize);
+    int caseSize;
+    std::cin >> caseSize;
 
-	for (int caseIndex = 1; caseIndex <= caseSize; ++caseIndex) {
-		process(caseIndex);
-	}
+    for (int caseIndex = 1; caseIndex <= caseSize; ++caseIndex) {
+        process(caseIndex);
+    }
 }
