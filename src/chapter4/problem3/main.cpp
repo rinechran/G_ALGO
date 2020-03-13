72af0339e59b87aa6788bec912cbb67382629188
rinechran <rinechran@outlook.jp>
2020-03-13 16:42:09 +0900

    feat : problem3

---
+++ src/chapter4/problem3/main.cpp
@@ -0,0 +1,51 @@
+#include <cstdio>
+
+using namespace std;
+
+
+/**
+ * a와 b의 최대 공약수를 계산하는 함수
+ *
+ * @param a
+ * @param b
+ * @return
+ */
+long long getGCD(long long a, long long b) {
+    while (a % b != 0) {
+        int temp = a % b;
+        a = b;
+        b = temp;
+    }
+    return b;
+}
+
+/**
+ * a와 b의 최소 공배수를 계산하는 함수
+ *
+ * @param a
+ * @param b
+ * @return
+ */
+long long getLCM(long long a, long long b) {
+    return a* b / getGCD(a, b);
+}
+
+void process(int caseIndex) {
+	int num1, num2;
+	scanf("%d%d", &num1, &num2);
+
+	long long gcd = getGCD(num1, num2);
+	long long lcm = getLCM(num1, num2);
+
+	printf("Case #%d:\n", caseIndex);
+	printf("%lld %lld\n", gcd, lcm);
+}
+
+int main() {
+	int caseSize;
+	scanf("%d", &caseSize);
+
+	for (int caseIndex = 1; caseIndex <= caseSize; ++caseIndex) {
+		process(caseIndex);
+	}
+}
