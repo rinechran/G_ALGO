5c75832267ff7eb46413ed5d8e8d2118f2eaf648
Eun Sik Jung <rinechran@outlook.jp>
2020-04-13 19:46:04 +0900

    feat : problem5

---
+++ src/chapter7/problem5/main.cpp
@@ -0,0 +1,40 @@
+﻿#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+
+
+template <class ForwardIt>
+void quicksort(ForwardIt first, ForwardIt last)
+{
+	if (first == last) return;
+	auto pivot = *std::next(first, std::distance(first, last) / 2);
+	ForwardIt middle1 = std::partition(first, last,
+		[pivot](const auto& em) { return em < pivot; });
+	ForwardIt middle2 = std::partition(middle1, last,
+		[pivot](const auto& em) { return !(pivot < em); });
+	quicksort(first, middle1);
+	quicksort(middle2, last);
+}
+
+
+int main() {
+	int N;
+	std::cin >> N;
+
+	vector<int> arr(N);
+	for (int i = 0; i < N; i += 1) {
+		std::cin >> arr[i];
+	}
+
+	quicksort(arr.begin(), arr.end());
+	for (int i = 0; i < N; i += 1) {
+		if (i > 0) {
+			printf(" ");
+		}
+		printf("%d", arr[i]);
+	}
+}
